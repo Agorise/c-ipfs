290e7f78296e6ee417c17fcda6dd7f43396eaccd
chronaeon <micahdameron@gmail.com>
2018-06-22 07:56:27 -0400

    deleted:    .gitignore 	new file:   c-libp2p 	new file:   c-multiaddr 	new file:   c-multihash 	new file:   c-protobuf 	new file:   lmdb 	new file:   sort.sh

---
+++ sort.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+	
+git clone https://www.github.com/chronaeon/c-ipfs.git
+cd c-ipfs
+		
+	git clone https://github.com/jmjatlanta/lmdb.git
+	git clone https://github.com/Agorise/c-protobuf.git
+	git clone https://github.com/Agorise/c-multihash.git
+	git clone https://github.com/Agorise/c-multiaddr.git
+	git clone https://github.com/Agorise/c-libp2p.git
+		
+	cd lmdb/libraries/liblmdb
+		make all
+		cd ..
+		cd ..
+		cd ..
+	cd c-protobuf
+		make all
+		cd ..
+	cd c-multihash
+		make all
+		cd ..
+	cd c-multiaddr
+		make all
+		cd ..
+	cd c-libp2p
+		cd ..
+make all
+		
