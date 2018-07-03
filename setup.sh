5ec298c3eb80061d1c29cafbfcd6264f1dca16f5
Micah Dameron <chronaeon@localhost.localdomain>
2018-07-03 17:46:02 -0400

    new file:   setup.sh 	deleted:    sort.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+	git submodule update --recursive --remote	
+		mv lmdb ../
+		mv c-protobuf ../
+		mv c-multihash ../
+		mv c-multiaddr ../
+		mv c-libp2p ../
+		cd ..	
+	cd lmdb/libraries/liblmdb
+		make all
+		cd ..
+		cd ..
+		cd ..
+	cd c-protobuf
+		make all
+		cd ..
+	cd c-multihash
+		make all
+		cd ..
+	cd c-multiaddr
+		make all
+		cd ..
+	cd c-libp2p
+		make all
+		cd ../c-ipfs/
+	make all
+
